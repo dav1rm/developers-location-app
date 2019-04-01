62f7fad8394ff65fe08dae4f38ab97a2612f3409
Davi <davirodrigues8@gmail.com>
2019-04-01 19:47:23 -0300

    Projeto base com eslint, reactotron babel root import editor config

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import '~/config/ReactotronConfig';
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    );
+  }
+}
